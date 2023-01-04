7b5087fb73408c9fdfc91daf92f5bb2361f95e7e
BENwada <domo0103wada@gmail.com>
2023-01-04 22:27:30 +0900

    first post

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+const formatDate = (date: string): string => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+export { formatDate };
