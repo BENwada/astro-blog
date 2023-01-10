b69c8556b7e4fec2961925bbf990c4e24ae44f26
BENwada <domo0103wada@gmail.com>
2023-01-10 13:21:01 +0900

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,20 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "https://benwada.simse.io",
   integrations: [
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
+    compress({ img: false }),
   ],
 });
---
+++ src/utils/capitalizeSentence.ts
@@ -0,0 +1,9 @@
+export const capitalizeSentence = (input: string): string => {
+  const words = input.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+
+  return words.join(" ");
+};
